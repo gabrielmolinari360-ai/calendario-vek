f4ef36c6d633f7e0f199dfb765cc55ea85b300f4
Gabriel Molinari <gabrielmolinari360@gmail.com>
2026-06-11 11:05:33 -0300

    fix: allow werkzeug in production for socketio

--- app.py
+++ app.py
@@ -150,4 +150,4 @@ def on_connect():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5050))
-    socketio.run(app, host="0.0.0.0", port=port, debug=False)
+    socketio.run(app, host="0.0.0.0", port=port, debug=False, allow_unsafe_werkzeug=True)
